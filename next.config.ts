9432e81af0a6aebf8fb81627403079ea6b24bd54
Karula-k <d.fikriirsyad@gmail.com>
2025-11-24 10:19:39 +0700

    add deploy

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,11 @@ const nextConfig: NextConfig = {
   // Configuration options
   experimental: {
     serverActions: {
-      allowedOrigins: ["localhost:3000", "localhost:3001"],
+      allowedOrigins: [
+        "localhost:3000",
+        "localhost:3001",
+        "https://nest.virsd.com",
+      ],
     },
   },
 };
